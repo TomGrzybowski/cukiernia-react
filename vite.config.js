e01a25632e7de864e931d5436ad183b4e22f8c96
TomekGrzybek <t.grzybowski94@gmail.com>
2023-06-20 17:35:30 +0200

    more setup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import copy from "vite-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [{ src: "src/assets", dest: "dist/assets" }],
+    }),
+  ],
   base: "/cukiernia-react/",
   // root: "./src",
   // build: {
