7260889bfed1bd162ff4b1a4274d4b54fe900371
TomekGrzybek <t.grzybowski94@gmail.com>
2023-06-20 18:03:01 +0200

    test logo from firebase

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,37 @@
 import styles from "./components.module.scss";
-// import logo from "../../assets/main-logo.png";
+import { useEffect, useState } from "react";
+import { ref, getDownloadURL } from "firebase/storage";
 
 import Socials from "../Socials/Socials.jsx";
 import MainNavbar from "../MainNavbar/MainNavbar.jsx";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import MobileMenu from "../MobileMenu/MobileMenu.jsx";
-import { useEffect, useState } from "react";
+import { storage } from "../../api/firebaseConfig.js";
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+  const [logoUrl, setLogoUrl] = useState("");
+
+  useEffect(() => {
+    // Fetch the image URLs from Firebase Storage
+    const fetchImageURLs = async () => {
+      try {
+        const storageRef = ref(storage, "assets/main-logo.png");
+        // const imageName = "main-logo.png"; // Replace with the specific image name
+
+        // Get the reference to the specific image
+        const url = await getDownloadURL(storageRef);
+
+        setLogoUrl(url);
+      } catch (error) {
+        console.error("Error fetching image URL:", error);
+      }
+    };
+
+    fetchImageURLs();
+  }, []);
 
   useEffect(() => {
     const handleResize = () => {
@@ -40,7 +61,7 @@ const Header = () => {
         {/* <MobileMenu /> */}
         <div className={styles.box}>
           <img
-            src="../../assets/main-logo.png"
+            src={logoUrl}
             alt="Logo cukierni"
             width={180}
             height={180}
