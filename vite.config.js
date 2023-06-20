c8b0eb0db55230eae481649d11198c3a40f4194e
TomekGrzybek <t.grzybowski94@gmail.com>
2023-06-20 14:19:01 +0200

    testy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: "./src",
+  build: {
+    rollupOptions: {
+      input: "./src/main.jsx", // Replace with your desired entry point file
+    },
+  },
+});
